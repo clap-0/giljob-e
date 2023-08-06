c86660cb75863a6027bead4cc727e55b9f72b101
clap_0 <clap-0@naver.com>
2023-08-06 22:08:45 +0900

    Initial commit

---
+++ src/main/java/com/likelion/giljobe/GiljobEApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.giljobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiljobEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiljobEApplication.class, args);
+	}
+
+}
