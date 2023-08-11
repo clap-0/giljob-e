b5ab2bf9bb5bd060d8c8686fc1b060fc5405d075
clap_0 <clap-0@naver.com>
2023-08-11 19:24:06 +0900

    fix: POST 요청 시 403 에러 발생 버그 수정

--- src/main/java/com/likelion/giljobe/config/SecurityConfig.java
+++ src/main/java/com/likelion/giljobe/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,6 +19,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers(
+                        new AntPathRequestMatcher("/**")
+                ).permitAll()
+                .and()
                 // CORS 설정
                 .cors(c -> {
                     CorsConfigurationSource source = request -> {
